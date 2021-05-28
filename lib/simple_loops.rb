dbb698258744adcbe5eb81f66b1cf9062c86920c
dashing-utility-e4d8 <eten.uket@yale.edu>
2021-05-28 23:26:51 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,27 @@
 # Write your methods here
+
+def loop_message_five_times (string)
+  5.times do
+  puts string 
+  end
+end
+loop_message_five_times("Hello World.")
+
+
+def loop_message_n_times(message, number)
+counter = 0 
+while counter < number do
+  puts message
+  counter += 1
+end
+end
+loop_message_n_times("Hello moon", 8)
+
+def output_array (message, number = 8)
+  counter = 0 
+while counter < number do
+  puts message
+  counter += 1
+end
+end
+output_array("Hello moon", number)
